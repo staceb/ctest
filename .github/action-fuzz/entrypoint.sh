357870f5d5c2f404739c94ae8172deb58f8ce15b
Stacey Burns <5744432+staceb@users.noreply.github.com>
2018-11-22 20:21:45 +0100

    Update entrypoint.sh

--- .github/action-fuzz/entrypoint.sh
+++ .github/action-fuzz/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -l
 
 sh -c "echo $*";
-git status;
 
+git grep -nF -C1 -e show-widgets -e dark-theme -e chatbox -e beta-ui -e
+set-logging-level -e database-connection -e caching-disabled -e
+cache-timeout -e autorelease -e auto-deploy-lambda -e on-default-branch -e
+autorelease-environments -e auto-deploy-env -e use-failover-database -e
+show-nps-survery -e new-metrics-feature;
 
